a91962a4c6524c39930f0b57ce4ab84654ad2178
victor-kenzie <victor@kenzie.com.br>
2022-05-25 09:48:32 -0300

    Add pasta js-base + códigos de funções

---
+++ js-base/src/js/funcoes.js
@@ -0,0 +1,54 @@
+/**
+ * Função Declarativa
+ * Nomeada
+ * Faz Hoisting
+ */
+function funcaoSoma(valor1, valor2) {
+    return valor1 + valor2;
+}
+
+/**
+ * Função como Valor
+ * Anônima
+ * Não faz Hoisting
+ */
+const funcaoSoma2 = function (valor1, valor2) {
+    return valor1 + valor2;
+}
+
+/**
+ * Arrow Function
+ * Anônima
+ * Não faz Hoisting
+ */
+const funcaoSoma3 = (valor1, valor2) => {
+    return valor1 + valor2
+}
+
+function mostrarNome() {
+    const nome = "Victor Augusto";
+
+    return nome;
+}
+
+function transformaNomeUpperCase() {
+    return mostrarNome().toUpperCase();
+}
+
+function transformaNomeLowerCase() {
+    return mostrarNome().toLowerCase();
+}
+
+const nomeMaiuscula = transformaNomeUpperCase();
+const nomeMinuscula = transformaNomeLowerCase();
+
+console.log({
+    nomeMaiuscula,
+    nomeMinuscula
+})
+
+
+
+
+
+
