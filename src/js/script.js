0826eeb60c2f1e60aa1122e145ec5a7692dd8a58
victor-kenzie <victor@kenzie.com.br>
2022-06-15 10:43:52 -0300

    Adiciona código tira-duvidas

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,36 @@ const carteiraVictor = new CNH("Victor Augusto", "12345678909");
 carteiraVictor.resetarMultas()
 
 // console.log(carteiraVictor)
+
+
+
+const stringJoin = ["Victor", "Augusto", "da", "Silva", "Lima"]
+
+function joinSimples(array, caracter) {
+    let resultado = array[0]
+    for (let contador = 1; contador < array.length; contador++) {
+        resultado += caracter + array[contador]
+    }
+
+    return resultado
+}
+
+// console.log(joinSimples(stringJoin, "-"))
+
+const listaTr = document.querySelectorAll("tbody tr")
+
+const teste = [...listaTr].filter((elem) => {
+    // elem.dataset.nome="Victor"
+    return elem.dataset.categoria === "saudavel"
+})
+
+const listaBotao = document.querySelectorAll("button");
+[...listaBotao].forEach((elem) => {
+    elem.addEventListener("click", function(event) {
+        const alvo = event.target;
+        const categoria = alvo.closest("tr").dataset.categoria
+
+        console.log({alvo, categoria})
+    })
+})
+
